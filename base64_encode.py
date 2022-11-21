40bb42be681c375a030ad5955fda762bd891f824
EndyXVIII <endyhysa18@gmail.com>
2022-11-21 22:49:17 +0100

    base64 encode

--- base64_encode.py
+++ base64_encode.py
@@ -1,7 +1,10 @@
 import base64
 
-def base64ToText(t):
+def base64ToText(t):    #Simply returns the text decoded 
     return base64.b64decode(t).decode('UTF-8')
 
-s = "UHl0aG9uIGlzIGZ1bg=="
-print(base64ToText(s))
+#------------------------------------Example#------------------------------------
+s = "UHl0aG9uIGlzIGZ1bg=="  
+print(base64ToText(s)) #Print result: Python is fun 
+
+#String example got from https://stackabuse.com/encoding-and-decoding-base64-strings-in-python/
