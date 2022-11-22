b22c7bee5824fe5ea63e9053000dff6b92686d21
EndyXVIII <endyhysa18@gmail.com>
2022-11-22 23:54:37 +0100

    Int to binary and vice versa
    Added binary/int to int/binary functions

---
+++ int_toBinary.py
@@ -0,0 +1,20 @@
+
+def intToBinary(number):    #returns the binary value of the number
+    return format(number,'b')
+
+#-------------------------------EXAMPLE#-------------------------------
+n = 100 
+print(intToBinary(n)) #Output: 1100100
+#----------------------------------------------------------------------
+
+
+def binaryToInt(number):
+    return int(str(number),2)   #Simply returns the integer number 
+
+
+#-------------------------------EXAMPLE--------------------------------
+n = 1101010111010100011001
+print(binaryToInt(n)) #Output: 3503385
+#----------------------------------------------------------------------
+
+
