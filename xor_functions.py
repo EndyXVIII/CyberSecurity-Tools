36cc98ddff258425d86162845767c5c1e59487dd
EndyXVIII <endyhysa18@gmail.com>
2022-11-23 22:43:17 +0100

    Xor Functions
    Deleted function that doesn't work

--- xor_functions.py
+++ xor_functions.py
@@ -7,12 +7,3 @@ num = 20
 key = 4
 print(xorInt(num, key)) #It prints 16
 #------------------------------------------------------------
-
-def xorBinary(nBinary, key):
-    y = nBinary ^ key
-    print(bin(y)[2:].zfill(len(nBinary)))
-
-
-s = 1001010101
-d = 10
-xorBinary(s, d)
